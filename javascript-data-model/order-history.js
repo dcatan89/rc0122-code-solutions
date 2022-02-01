bc8075f34f07b54e64a3f26e8dd69de2bcfce3e0
Daniel Catan <danieljcatan@gmail.com>
2022-02-01 10:27:19 -0800

    in progress

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,18 @@
+var orderHistory = [
+  {
+    orderDate: 'Aug 4, 2020',
+    totalPrice: '$34.00',
+    Shipto: 'JS MASHER',
+    Ordernumber: '114-39414689-8772232',
+    deliverDate: 'Aug 8, 2020',
+    items: [
+      {
+        type: 'book',
+        name: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr.Axel'
+
+      }
+    ]
+  }
+
+];
