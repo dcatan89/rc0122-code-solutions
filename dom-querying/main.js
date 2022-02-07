9b168a1c212b109d42efc5b6bf97906897585ad8
Daniel Catan <danieljcatan@gmail.com>
2022-02-07 10:18:13 -0800

    dom-querying complete

---
+++ dom-querying/main.js
@@ -0,0 +1,24 @@
+console.log('hello, world');
+
+var $h1 = document.querySelector('h1');
+
+console.log('value of $h1:', $h1);
+
+console.dir($h1);
+
+var $explanationID = document.querySelector('#explanation');
+
+console.log('value of $explanationID:', $explanationID);
+
+console.dir($explanationID);
+
+var $hint = document.querySelector('.hint');
+console.log('value of $hint:', $hint);
+console.dir($hint);
+
+var $pNodes = document.querySelectorAll('p');
+console.log('value of  $pNodes', $pNodes);
+
+var $examLink = document.querySelectorAll('.example-link');
+
+console.log('value of $examLink:', $examLink);
