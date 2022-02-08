680b40fe270a3e1ada883aa19e65590df3b2a9b3
Daniel Catan <danieljcatan@gmail.com>
2022-02-08 14:33:55 -0800

    javascript-typing-tutor complete

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,18 +1,19 @@
 var $spanList = document.querySelectorAll('span');
+var i = 0;
 
 function typingTutor(event) {
-  for (var i = 0; i < $spanList.length; i++) {
-    if (event.key !== $spanList[i].textContent) {
-      $spanList[i].className = 'wrong';
-    }
-    if (event.key === $spanList[i].textContent) {
-      if ($spanList[i].textContent === '&ThinSpace;') {
-        i++;
-      }
-      $spanList[i].classList.add('correct');
-      $spanList[i + 1].className = 'current-letter';
-    }
+  if ($spanList[i].className === 'space current-letter') {
+    $spanList[i].className = 'space';
+    i++;
+  }
+  if (event.key !== $spanList[i].textContent) {
+    $spanList[i].classList.add('wrong');
+    return i;
+  }
+  if (event.key === $spanList[i].textContent) {
+    $spanList[i].className = 'correct';
+    $spanList[i + 1].classList.add('current-letter');
+    i++;
   }
 }
-
 document.addEventListener('keydown', typingTutor);
