6274a3a03243072c0ddc76358938867b3694768c
Daniel Catan <danieljcatan@gmail.com>
2022-02-22 11:11:29 -0800

    js constructors completed

---
+++ javascript-constructors/example-constructor.js
@@ -0,0 +1,11 @@
+function ExampleConstructor() {
+
+}
+
+console.log('value of ExampleConstructor:', ExampleConstructor);
+console.log('typeof ExampleConstructor:', typeof ExampleConstructor);
+
+var example = new ExampleConstructor();
+console.log('example:', example);
+
+console.log('example instanceof ExampleConstructor', example instanceof ExampleConstructor);
