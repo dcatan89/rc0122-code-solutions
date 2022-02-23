6a05e7c9a2a24f431c41e78b8c1b591adbfccef5
Daniel Catan <danieljcatan@gmail.com>
2022-02-23 12:20:14 -0800

    total assets prototype finished

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -16,3 +16,25 @@ Bank.prototype.openAccount = function (holder, balance) {
     return this.nextAccountNumber - 1;
   }
 };
+
+Bank.prototype.getAccount = function (number) {
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i].number === number) {
+      return this.accounts[i];
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+  var totalAccount = 0;
+  if (this.accounts.length === 0) {
+    return 0;
+  } else {
+    for (var i = 0; i < this.accounts.length; i++) {
+      var account = this.accounts[i].getBalance();
+      totalAccount = totalAccount + account;
+    }
+  }
+  return totalAccount;
+};
