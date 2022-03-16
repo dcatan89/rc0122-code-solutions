9db855140118c7284dd5fb339f5029e876d1a97b
dcatan89 <danieljcatan@gmail.com>
2022-03-16 20:36:20 +0000

    cleaned up code

--- node-require/calculate.js
+++ node-require/calculate.js
@@ -3,18 +3,22 @@ const subtract = require('./subtract');
 const divide = require('./divide');
 const multiply = require('./multiply');
 
-if (process.argv[3] === ('plus' || '+')) {
-  const sum = add(parseFloat(process.argv[2]), parseFloat(process.argv[4]));
-  console.log(`${process.argv[2]} + ${process.argv[4]} = ${sum}`);
-} else if (process.argv[3] === ('minus' || '-')) {
-  const difference = subtract(parseFloat(process.argv[2]), parseFloat(process.argv[4]));
-  console.log(`${process.argv[2]} - ${process.argv[4]} = ${difference}`);
-} else if (process.argv[3] === ('divide' || '/')) {
-  const quotient = divide(parseFloat(process.argv[2]), parseFloat(process.argv[4]));
-  console.log(`${process.argv[2]} / ${process.argv[4]} = ${quotient}`);
-} else if (process.argv[3] === ('multiply' || '*')) {
-  const multiplied = multiply(parseFloat(process.argv[2]), parseFloat(process.argv[4]));
-  console.log(`${process.argv[2]} x ${process.argv[4]} = ${multiplied}`);
+const num1 = parseFloat(process.argv[2]);
+const operator = process.argv[3];
+const num2 = parseFloat(process.argv[4]);
+
+if (operator === ('plus' || '+')) {
+  const sum = add(num1, num2);
+  console.log(`${num1} + ${num2} = ${sum}`);
+} else if (operator === ('minus' || '-')) {
+  const difference = subtract(num1, num2);
+  console.log(`${num1} - ${num2} = ${difference}`);
+} else if (operator === ('divide' || '/')) {
+  const quotient = divide(num1, num2);
+  console.log(`${num1} / ${num2} = ${quotient}`);
+} else if (operator === ('multiply' || '*')) {
+  const multiplied = multiply(num1, num2);
+  console.log(`${num1} x ${num2} = ${multiplied}`);
 } else {
   console.log('Unknown operator');
 }
