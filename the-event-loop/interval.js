c39a7633682114311d25234429ab3904d7afc0e1
dcatan89 <danieljcatan@gmail.com>
2022-03-16 22:39:50 +0000

    console.log preferences

--- the-event-loop/interval.js
+++ the-event-loop/interval.js
@@ -1,9 +1,9 @@
 let currentCount = 3;
 
 const intervalID = setInterval(() => {
-  console.log(currentCount);
   currentCount--;
-  if (currentCount < 0) {
+  console.log(currentCount);
+  if (currentCount === 0) {
     console.log('Blast off!');
     clearInterval(intervalID);
   }
