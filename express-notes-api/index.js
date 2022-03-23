df3040b17ff4af8f9d2e89c81d05ef496d7bd27f
dcatan89 <danieljcatan@gmail.com>
2022-03-23 20:06:58 +0000

    get with id params works with all status responses

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const data = require('./data.json');
 
+app.use(express.json());
+
 app.get('/api/notes', (req, res) => {
   const array = [];
   if (`${data.notes}` === {}) {
@@ -14,6 +16,21 @@ app.get('/api/notes', (req, res) => {
   res.json(array);
 });
 
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  if (Number.isNaN(id) || !Number.isInteger(id) || id < 0) {
+    res.status(400).json({
+      error: 'id must be a positive integer'
+    });
+  } else if (!data.notes[id]) {
+    res.status(404).json({
+      error: `cannot find note with id ${id}`
+    });
+  } else {
+    res.json(data.notes[id]);
+  }
+}
+);
 app.listen(3000, (req, res) => {
   // eslint-disable-next-line no-console
   console.log('3000 port Running');
