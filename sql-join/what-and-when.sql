62522c920f195a04ac1d5b82bfab4549900e2350
dcatan89 <danieljcatan@gmail.com>
2022-03-24 23:08:38 +0000

    sql join completed

---
+++ sql-join/what-and-when.sql
@@ -0,0 +1,6 @@
+select "releaseYear",
+       "g"."name" as "genre"
+  from "films"
+  join "filmGenre" using ("filmId")
+  join "genres" as "g" using ("genreId")
+where "title" = 'Boogie Amelie';
---
+++ sql-join/where-are-they.sql
@@ -0,0 +1,7 @@
+select "line1",
+       "c"."name" as "City",
+       "district",
+       "count". "name" as "Country"
+  from "addresses"
+  join "cities" as "c" using ("cityId")
+  join "countries" as "count" using("countryId");
