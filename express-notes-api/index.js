93c3a1d231c1eef32ccbdf3b4c2cdc7789c5b913
dcatan89 <danieljcatan@gmail.com>
2022-03-24 15:49:52 +0000

    put feature is working

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 const data = require('./data.json');
-
 app.use(express.json());
 
 app.get('/api/notes', (req, res) => {
@@ -56,6 +55,66 @@ app.post('/api/notes', (req, res) => {
     }
   });
 });
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  if (Number.isNaN(id) || !Number.isInteger(id) || id < 0) {
+    res.status(400).json({
+      error: 'id must be a positive integer'
+    });
+    return;
+  }
+  if (!data.notes[id]) {
+    res.status(404).json({
+      error: `cannot find note with id ${id}`
+    });
+    return;
+  }
+
+  delete data.notes[id];
+  fs.writeFile('data.json', `${JSON.stringify(data, null, 2)}`, err => {
+    if (err) {
+      console.error(res.status(500).json({
+        error: 'An unexpected error occurred.'
+      })
+      );
+    } else {
+      res.sendStatus(204);
+    }
+  });
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  const note = req.body;
+  const id = Number(req.params.id);
+  if (Number.isNaN(id) || !Number.isInteger(id) || id < 0 || !note.content) {
+    res.status(400).json({
+      error: 'id must be a positive integer'
+    });
+    return;
+  }
+
+  if (!data.notes[id]) {
+    res.status(404).json({
+      error: `cannot find note with id ${id}`
+    });
+    return;
+  }
+
+  data.notes[id].content = note.content;
+  fs.writeFile('data.json', `${JSON.stringify(data, null, 2)}`, err => {
+    if (err) {
+      console.error(res.status(500).json({
+        error: 'An unexpected error occurred.'
+      })
+      );
+    } else {
+      res.send(data.notes[id]);
+    }
+  });
+
+});
+
 app.listen(3000, (req, res) => {
   // eslint-disable-next-line no-console
   console.log('3000 port Running');
