9d50bc876b52e2794fafe80883996b525e2ae6ba
dcatan89 <danieljcatan@gmail.com>
2022-04-02 01:05:09 +0000

    remembered how to use props

--- react-rendering-lists/src/index.jsx
+++ react-rendering-lists/src/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Pokemon() {
-  const pokedex = [
-    { number: '001', name: 'Bulbasaur' },
-    { number: '004', name: 'Charmander' },
-    { number: '007', name: 'Squirtle' },
-    { number: '025', name: 'Pikachu' },
-    { number: '039', name: 'Jigglypuff' }
-  ];
-  const listPoke = pokedex.map(pokedex =>
+const pokedex = [
+  { number: '001', name: 'Bulbasaur' },
+  { number: '004', name: 'Charmander' },
+  { number: '007', name: 'Squirtle' },
+  { number: '025', name: 'Pikachu' },
+  { number: '039', name: 'Jigglypuff' }
+];
+function Pokemon(props) {
+  const poke = props.pokedex;
+  const listPoke = poke.map(pokedex =>
     <li key={pokedex.name}>{pokedex.name}</li>
   );
   return (
@@ -18,6 +19,6 @@ function Pokemon() {
 }
 
 ReactDOM.render(
-  <Pokemon />,
+  <Pokemon pokedex={pokedex}/>,
   document.querySelector('#root')
 );
