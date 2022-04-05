d9ebc0e668308ab400a39397b4ea048bab5f7b48
dcatan89 <danieljcatan@gmail.com>
2022-04-05 19:47:42 +0000

    cleaned up code and css, functionality still working

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -8,24 +8,18 @@ class Accordion extends React.Component {
   }
 
   handlePopUp(id) {
-    if (this.state.topicId === id) {
-      this.setState({ topicId: null });
-    } else {
-      this.setState({ topicId: id });
-    }
+    this.state.topicId === id
+      ? this.setState({ topicId: null })
+      : this.setState({ topicId: id });
   }
 
   render() {
-    let name = null;
     const { topicId } = this.state;
     const randomdex = this.props.dex;
     const listDex = randomdex.map(dex => {
-      if (topicId === dex.id) {
-        name = dex.name;
-      }
-      return <div className="row" key={dex.id}>
-        <h2 className="lgrey-bgc topics col-full " onClick={() => { this.handlePopUp(dex.id); }}>{dex.topic}</h2>
-        <p className="full-height col-full" >{name}</p>
+      return <div className="" key={dex.id}>
+        <h2 className="lgrey-bgc topics" onClick={() => { this.handlePopUp(dex.id); }}>{dex.topic}</h2>
+        <p className="names" >{topicId === dex.id ? dex.name : null}</p>
       </div>;
     }
     );
