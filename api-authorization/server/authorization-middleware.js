00969a81c384e756fa7443a2a1b1bf660b957ddb
dcatan89 <danieljcatan@gmail.com>
2022-04-08 05:59:42 +0000

    removed try and catch as it was not needed

--- api-authorization/server/authorization-middleware.js
+++ api-authorization/server/authorization-middleware.js
@@ -3,18 +3,15 @@ const ClientError = require('./client-error'); // eslint-disable-line
 
 function authorizationMiddleware(req, res, next) {
   /* your code here */
-  try {
-    const token = req.header('X-Access-Token');
-    if (!token) {
-      throw new ClientError(401, 'invalid access token');
-    }
-    const payload = jwt.verify(token, process.env.TOKEN_SECRET);
-    req.user = payload;
-    next();
-  } catch (err) {
-    console.error(err);
+  const token = req.header('X-Access-Token');
+  if (!token) {
+    throw new ClientError(401, 'invalid access token');
   }
-  /**
+  const payload = jwt.verify(token, process.env.TOKEN_SECRET);
+  req.user = payload;
+  next();
+}
+/**
    * Try to get the 'X-Access-Token' from the request headers.
    * If no token is provided,
    *   throw a 401 error with the message 'authentication required'
@@ -22,12 +19,11 @@ function authorizationMiddleware(req, res, next) {
    * Assign the extracted payload to the user property of the req object.
    * Call next() (with no arguments) to let Express know to advance to its next route or middleware.
    */
-  /**
+/**
     * References:
     * https://expressjs.com/en/4x/api.html#req.get
     * https://nodejs.org/api/http.html#http_message_headers
     * https://github.com/auth0/node-jsonwebtoken#jwtverifytoken-secretorpublickey-options-callback
     */
-}
 
 module.exports = authorizationMiddleware;
